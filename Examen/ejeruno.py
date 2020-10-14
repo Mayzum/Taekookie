94fe07063b78773be33a89e3c3251d966dbfe2a5
Mayzum <vegaapaza23@gmail.com>
2020-10-14 19:48:31 +0000

    one

---
+++ Examen/ejeruno.py
@@ -0,0 +1,16 @@
+def PromFinalNMVA():
+    #datos de entrada
+    str
+    nota1=int(input("ingresar nota:"))
+    nota2=int(input("ingresar nota"))
+    nota3=int(input("ingresar nota"))
+    nota4=int(input("ingresar nota"))
+    #proceso
+    U1=nota1*0.1
+    U2=nota2*0.15
+    U3=nota3*0.25
+    U4=nota4*0.5
+    promedio = U1+U2+U3+U4
+    #datos de salida
+    print("tu promedio es:",promedio)
+PromFinalNMVA() 
