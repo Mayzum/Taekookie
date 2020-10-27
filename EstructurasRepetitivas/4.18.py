146eca8aa503874823facf1bccb937a0e1c380d6
Mayzum <vegaapaza23@gmail.com>
2020-10-27 19:39:55 +0000

    ejercicio4.18

---
+++ EstructurasRepetitivas/4.18.py
@@ -0,0 +1,10 @@
+def TierrasVendidasNMVA():
+ anhos= 2020-1961
+ inversion=15.0/100.0
+ pago=1500.0
+ ahorros=pago*(1.0+inversion)**(anhos)
+ print ("El valor de pago es:",pago)
+ print ("El valor de ahorro es:",ahorros)
+ print ("El valor de inversion es:",inversion)
+ print ("El tiempo de anhos es:",anhos)
+TierrasVendidasNMVA()
