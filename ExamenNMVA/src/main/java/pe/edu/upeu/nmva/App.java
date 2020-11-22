be2eac3ee9373d1da702ba05f80934aeb6616f61
Mayzum <vegaapaza23@gmail.com>
2020-11-22 01:00:11 +0000

    ExamenNMVA

---
+++ ExamenNMVA/src/main/java/pe/edu/upeu/nmva/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.nmva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
